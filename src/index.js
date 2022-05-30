d0f7b577f057b960dff0f2d7bcd2acbbceaeea45
syfq-isml <macamyesjer@gmail.com>
2022-05-31 01:27:00 +0800

    add DOM interaction
     - x and o appears correctly

--- src/index.js
+++ src/index.js
@@ -1,8 +1,80 @@
 import "../node_modules/normalize.css/normalize.css";
 import "./stylesheets/style.css";
 
-import { initGameboards } from "./scripts/DOMmodule";
+import {
+	initGameboards,
+	addListenersToTiles,
+	removeListenersFromTiles,
+	DOM_attack,
+	ANNOUNCE_gameOver,
+	ANNOUNCE_shipHit,
+	ANNOUNCE_shipMiss,
+	RENDER_shipHit,
+	RENDER_shipMiss,
+} from "./scripts/DOMmodule";
+import { Ship } from "./scripts/shipFactory";
+import { humanPlayer, computerPlayer } from "./scripts/humanPlayer";
+
+let computerTurn;
 
 initGameboards();
+runNewGame();
 
-console.log("Setup ok!");
+function runNewGame() {
+	spawnShips(humanPlayer);
+	spawnShips(computerPlayer);
+	computerTurn = false;
+}
+
+// function to get input from human
+addListenersToTiles(handleHumanClick);
+
+function handleHumanClick(e) {
+	let result = DOM_attack(e);
+	if (result === "A ship was hit!") {
+		RENDER_shipHit(e);
+		ANNOUNCE_shipHit(result);
+		return;
+	}
+
+	if (result === "Missed...") {
+		RENDER_shipMiss(e);
+		ANNOUNCE_shipMiss(result);
+	}
+
+	if (computerPlayer.gboard.isAllSunk()) {
+		ANNOUNCE_gameOver();
+		return;
+	}
+}
+
+function spawnShips(player) {
+	const Ship_5 = Ship(5);
+	const Ship_4 = Ship(4);
+	const Ship_3_1 = Ship(3);
+	const Ship_3_2 = Ship(3);
+	const Ship_2_1 = Ship(2);
+	const Ship_2_2 = Ship(2);
+	player.gboard.placeShipOnGameboard(Ship_5, 0, 0, "vertical");
+	player.gboard.placeShipOnGameboard(Ship_4, 4, 3, "horizontal");
+	player.gboard.placeShipOnGameboard(Ship_3_1, 7, 1, "horizontal");
+	player.gboard.placeShipOnGameboard(Ship_3_2, 6, 5, "vertical");
+	player.gboard.placeShipOnGameboard(Ship_2_1, 8, 8, "horizontal");
+	player.gboard.placeShipOnGameboard(Ship_2_2, 9, 8, "horizontal");
+}
+
+// after each click
+// do the attack ==== DOM_attack
+// check if all is sunk --> YES: game over, NO: continue ====  DATA:  // DOM: ANNOUNCE_gameOver
+// check if a ship is hit --> render YES: X, NO: O + announcement
+// check if any ship is sunk --> render YES: all red, NO: continue + announcement
+// check if a ship was hit --> YES: dont change turn, NO: change turn + announcement
+// remove event listeners
+
+// computer's turn
+// computer does a random attack
+// check if all is sunk --> YES: game over, NO: continue
+// check if a ship is hit --> render YES: X, NO: O + announcement
+// check if any ship is sunk --> render YES: all red, NO: continue + announcement
+// check if a ship was hit --> YES: dont change turn, NO: change turn
+// add event listeners
--- src/scripts/DOMmodule.js
+++ src/scripts/DOMmodule.js
@@ -1,4 +1,6 @@
-const createGameboard = (divID) => {
+import { humanPlayer, computerPlayer } from "./humanPlayer";
+
+const createGameboard = (divID, playerName) => {
 	// create a 10x10 grid
 	// --> create 100 divs and append to the div
 	const parent = document.querySelector(divID);
@@ -8,17 +10,80 @@ const createGameboard = (divID) => {
 
 		const tileContent = document.createElement("div");
 		tileContent.classList.add("tile-content");
-		tileContent.setAttribute("data-tile-id", `${i + 1}`);
+		if (i < 10) {
+			tileContent.setAttribute("data-tile-id", `0${i}`);
+		} else {
+			tileContent.setAttribute("data-tile-id", `${i}`);
+		}
+		tileContent.setAttribute("data-player", playerName);
 
-		tileContent.innerText = "X";
+		// tileContent.innerText = "X";
 		tile.append(tileContent);
 		parent.append(tile);
 	}
 };
 
 const initGameboards = () => {
-	createGameboard("#player-gameboard");
-	createGameboard("#computer-gameboard");
+	createGameboard("#player-gameboard", "human");
+	createGameboard("#computer-gameboard", "computer");
 };
 
-export { initGameboards };
+const addListenersToTiles = (callback) => {
+	const tiles = document.querySelectorAll(`[data-player="computer"]`);
+	tiles.forEach((tile) => {
+		tile.addEventListener("click", callback);
+	});
+};
+
+const DOM_attack = (e) => {
+	// get the tile coordinate
+	const coords = e.target.dataset.tileId;
+	let arr = Array.from(coords);
+	let [x, y] = [...arr];
+	return humanPlayer.makeAttack(x, y, computerPlayer.gboard);
+};
+
+const removeListenersFromTiles = (player, callback) => {
+	const tiles = document.querySelectorAll(`[data-tile-id="${player}]"`);
+	tiles.forEach((tile) => {
+		tile.removeEventListener("click", callback);
+	});
+};
+
+const ANNOUNCE_gameOver = () => {
+	const ann = document.querySelector(".instructions p");
+	ann.innerText = "GAME OVER!";
+};
+
+const ANNOUNCE_shipHit = (str) => {
+	const ann = document.querySelector(".instructions p");
+	ann.innerText = str;
+};
+const ANNOUNCE_shipMiss = (str) => {
+	const ann = document.querySelector(".instructions p");
+	ann.innerText = str;
+};
+
+const RENDER_shipHit = (e) => {
+	const tile = e.target;
+	tile.innerText = "X";
+	tile.classList.add("tile-hit");
+};
+
+const RENDER_shipMiss = (e) => {
+	const tile = e.target;
+	tile.innerText = "o";
+	tile.classList.add("tile-miss");
+};
+
+export {
+	initGameboards,
+	addListenersToTiles,
+	removeListenersFromTiles,
+	DOM_attack,
+	ANNOUNCE_gameOver,
+	ANNOUNCE_shipHit,
+	ANNOUNCE_shipMiss,
+	RENDER_shipHit,
+	RENDER_shipMiss,
+};
